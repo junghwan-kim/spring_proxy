59ad569ae47c864e813daf2e194cb752e750968a
김중환 <hairmetal@megastudy.net>
2025-03-13 09:52:55 +0900

    인터페이스기반 프록시/구체클랙스기반 프록시

---
+++ src/test/java/hello/proxy/prueproxy/concreateproxy/code/ConcreteClient.java
@@ -0,0 +1,15 @@
+package hello.proxy.prueproxy.concreateproxy.code;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcreteClient {
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute(){
+        concreteLogic.operation();
+    }
+}
